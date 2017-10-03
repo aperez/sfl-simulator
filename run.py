8a861b0754bf762d3c341933bf5c79840aff351d
Alexandre Perez <alexandrecperez@gmail.com>
2017-10-03 10:40:30 +0100

    Generate topologies with DendroPy.

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import simulator
 
 if __name__ == "__main__":
-    topology = simulator.Topology()
+    generator = simulator.TopologyGenerator()
+    topology = generator.generate()
+    topology.print_plot()
--- simulator/topology.py
+++ simulator/topology.py
@@ -1,4 +1,14 @@
+import dendropy.simulate as treesim
 
-class Topology(object):
-    def __init__(self):
-        pass
+class TopologyGenerator(object):
+    def __init__(self, components=10):
+        self.components = components
+        self.default_args = {'birth_rate': 1.0, 'death_rate': 0.5}
+
+    def generate(self, **kwargs):
+        for key,value in self.default_args.items():
+            if key not in kwargs:
+                kwargs[key] = value
+        kwargs['num_extant_tips'] = self.components
+
+        return treesim.birth_death_tree(**kwargs)
